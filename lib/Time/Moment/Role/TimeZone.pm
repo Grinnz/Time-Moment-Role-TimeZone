2493b2597fdef8eeb467522872514ad9ecef9fa5
Dan Book <grinnz@grinnz.com>
2018-03-26 16:23:03 -0400

    add systemtime methods (chansen)

--- lib/Time/Moment/Role/TimeZone.pm
+++ lib/Time/Moment/Role/TimeZone.pm
@@ -2,10 +2,11 @@ package Time::Moment::Role::TimeZone;
 
 use Role::Tiny;
 use Carp ();
+use Time::Local ();
 
 our $VERSION = '0.002';
 
-requires qw(with_offset_same_instant with_offset_same_local
+requires qw(epoch offset with_offset_same_instant with_offset_same_local
   utc_rd_as_seconds utc_rd_values local_rd_as_seconds local_rd_values utc_year);
 
 sub with_time_zone_offset_same_instant {
@@ -20,6 +21,20 @@ sub with_time_zone_offset_same_local {
   return $self->with_offset_same_local($tz->offset_for_local_datetime($self) / 60);
 }
 
+sub with_systemtime_offset_same_instant {
+  my ($self) = @_;
+  my $time = $self->epoch;
+  my $offset = Time::Local::timegm(localtime $time) - $time;
+  return $self->with_offset_same_instant($offset / 60);
+}
+
+sub with_systemtime_offset_same_local {
+  my ($self) = @_;
+  my $time = $self->epoch + $self->offset * 60;
+  my $offset = $time - Time::Local::timelocal(gmtime $time);
+  return $self->with_offset_same_local($offset / 60);
+}
+
 1;
 
 =head1 NAME
@@ -28,21 +43,25 @@ Time::Moment::Role::TimeZone - Adjust Time::Moment with time zone objects
 
 =head1 SYNOPSIS
 
+  use Time::Moment;
   use Role::Tiny ();
   use DateTime::TimeZone;
 
-  my $tz = DateTime::TimeZone->new(name => 'America/New_York');
   my $class = Role::Tiny->create_class_with_roles('Time::Moment', 'Time::Moment::Role::TimeZone');
+
+  my $tz = DateTime::TimeZone->new(name => 'America/New_York');
   my $tm = $class->from_epoch(1000212360)->with_time_zone_offset_same_instant($tz);
 
-  use Time::Moment;
-  use Role::Tiny ();
   use DateTime::TimeZone::Olson 'olson_tz';
 
   my $tz = olson_tz('Europe/Oslo');
   my $tm = Time::Moment->new(year => 2012, month => 3, day => 4, hour => 13, minute => 7, second => 42);
   Role::Tiny->apply_roles_to_object($tm, 'Time::Moment::Role::TimeZone');
   $tm = $tm->with_time_zone_offset_same_local($tz);
+
+  my $tm = $class->from_epoch(1522095272)->with_systemtime_offset_same_instant;
+
+  my $tm = $class->new_from_string('2009-05-02T12:15:30Z')->with_systemtime_offset_same_local;
 
 =head1 DESCRIPTION
 
@@ -67,6 +86,20 @@ according to the given time zone object at that instant.
 Returns a L<Time::Moment> of the same local time, with an offset from UTC
 according to the given time zone object at that local time.
 
+=head2 with_systemtime_offset_same_instant
+
+  my $same_instant = $tm->with_systemtime_offset_same_instant;
+
+As in L</"with_time_zone_offset_same_instant">, but using the system local time
+zone.
+
+=head2 with_systemtime_offset_same_local
+
+  my $same_local = $tm->with_systemtime_offset_same_local;
+
+As in L</"with_time_zone_offset_same_local">, but using the system local time
+zone.
+
 =head1 CAVEATS
 
 L<Time::Moment> does not store a time zone; these methods only set the offset
@@ -78,10 +111,13 @@ time.
   my $tm = $class->now->with_time_zone_offset_same_instant($tz); # now in $tz
   my $next_day = $tm->plus_days(1)->with_time_zone_offset_same_local($tz); # 1 day from now in $tz
   my $24h_later = $tm->plus_days(1)->with_time_zone_offset_same_instant($tz); # 86400 seconds from now in $tz
+  my $next_day_system = $tm->plus_days(1)->with_systemtime_offset_same_local; # 1 day from now in system local time
+  my $24h_later_system = $tm->plus_days(1)->with_systemtime_offset_same_instant; # 86400 seconds from now in system local time
 
-Note that C<with_time_zone_offset_same_local> may throw an exception here if
+Note that L</"with_time_zone_offset_same_local"> may throw an exception here if
 the new local time does not exist in that time zone (e.g. between 2 and 3 AM on
-the start of Daylight Savings Time).
+the start of Daylight Savings Time). L</"with_systemtime_offset_same_local">
+(using L<Time::Local>) will return the time one hour later if this occurs.
 
 =head1 BUGS
 
@@ -90,6 +126,10 @@ Report any issues on the public bugtracker.
 =head1 AUTHOR
 
 Dan Book <dbook@cpan.org>
+
+=head1 CREDITS
+
+Christian Hansen <chansen@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
